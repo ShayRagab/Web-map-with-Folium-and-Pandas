536706b6f5b2944687813bca4fd868c8b44c8afe
Shay <38939204+ShayRagab@users.noreply.github.com>
2019-04-05 10:36:11 +0200

    Add files via upload

--- webmap.py
+++ webmap.py
@@ -19,12 +19,24 @@ def color_producer(elevation):
 
 map = folium.Map(location=[34.073636, -118.400195], zoom_start=6, tiles="Mapbox Bright")
 
-fg=folium.FeatureGroup(name="My Map")
+# Volcanoes marker layer
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
     # for circle marker
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-map.add_child(fg)
+# Population layer
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000
+else "orange" if x["properties"]["POP2005"] < 20000000 else "red"}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 
 map.save("webmap.html")
